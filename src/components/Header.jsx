7460c800c744bab9cad7db952d52c6e4903067d5
SARVESH-SAKSENA <sarvsak04@gmail.com>
2025-08-20 19:02:57 +0530

    signin/signup form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
+import { LOGO } from "../utils/constant";
+
 const Header = () =>  {
     return (
-        <div> Header</div>
-    )
+      <div className="bg-gradient-to-b from-black absolute z-10 w-full">
+        <img
+          src={LOGO}
+          alt="logo"
+          className="w-52"
+        ></img>
+      </div>
+    );
 };
 export default Header;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "./Header";
+import { LOGIN_BG } from "../utils/constant";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [isSignInForm, setIsSignInForm] = useState(true);
 
-export default Login
+  const toggleSignIn = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img src={LOGIN_BG} alt="bg" className="w-full h-full"></img>
+      </div>
+      <form className="bg-black absolute w-4/12 p-12 mx-auto right-0 left-0 my-36 text-white rounded-lg bg-opacity-80">
+        <h1 className="text-bold text-4xl pb-12 pt-6">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Name"
+            className="p-3 bg-gray-700 w-full my-3 rounded-lg"
+          ></input>
+        )}
+        <input
+          type="text"
+          placeholder="E-mail id or phone no."
+          className="p-3 bg-gray-700 w-full my-3 rounded-lg"
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 bg-gray-700 w-full my-3 rounded-lg"
+        ></input>
+        <input type="checkbox" className="accent-red-600 my-6" />
+        <label className="px-1 my-6">Remember me</label>
+        <button className="bg-red-600 w-full p-3 rounded-lg opacity-100">
+          Sign in
+        </button>
+        <p onClick={toggleSignIn} className="cursor-pointer my-6">
+          New to Netflix? Sign Up now.
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
